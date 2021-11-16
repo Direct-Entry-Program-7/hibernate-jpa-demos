4c03d8928312bede54a8d94462599385118be567
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-16 16:39:17 +0530

    Create the second bootstrapping module

---
+++ 2-bootstrapping/src/main/java/dep7/HibernateDemo.java
@@ -0,0 +1,18 @@
+package dep7;
+
+import lk.ijse.dep7.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class HibernateDemo {
+
+    public static void main(String[] args) {
+
+        try(SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            Session session = sessionFactory.openSession()){
+
+            System.out.println(session);
+        }
+
+    }
+}
