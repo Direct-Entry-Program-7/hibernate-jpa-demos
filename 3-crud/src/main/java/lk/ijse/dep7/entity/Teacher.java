1c9400cccad7aefd08a22b89ad837cd26eb9ed16
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-19 11:35:39 +0530

    Add Teacher Entity

---
+++ 3-crud/src/main/java/lk/ijse/dep7/entity/Teacher.java
@@ -0,0 +1,63 @@
+package lk.ijse.dep7.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "teacher")
+public class Teacher implements Serializable {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+    private String name;
+    @Column(name = "contact_number")
+    private String contactNumber;
+
+    public Teacher() {
+    }
+
+    public Teacher(String name, String contactNumber) {
+        this.name = name;
+        this.contactNumber = contactNumber;
+    }
+
+    public Teacher(int id, String name, String contactNumber) {
+        this.id = id;
+        this.name = name;
+        this.contactNumber = contactNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", contactNumber='" + contactNumber + '\'' +
+                '}';
+    }
+}
