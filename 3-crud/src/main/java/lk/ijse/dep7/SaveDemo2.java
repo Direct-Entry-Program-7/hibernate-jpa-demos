d86d025cb3d447b6ae3fa5e4f1b5e5d93dd85d96
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-19 13:00:05 +0530

    Update SaveDemo2

--- 3-crud/src/main/java/lk/ijse/dep7/SaveDemo2.java
+++ 3-crud/src/main/java/lk/ijse/dep7/SaveDemo2.java
@@ -27,33 +27,49 @@ public class SaveDemo2 {
 //            System.out.println("Is Sovis inside the cache? " + session.contains(sovis));
 
             /* With persist() you can't specify an id for the auto generated primary key */
+
+            // transient x -> detach
+
 //            Teacher dinusha = new Teacher(10, "Dinusha", "077-456789123");
 //            System.out.println(dinusha);
 //            session.persist(dinusha);
 //            System.out.println(dinusha);
 //            System.out.println("Is dinusha inside the cache? " + session.contains(dinusha));
 
-            Teacher aruni = new Teacher("Aruni", "011-1234567");
-            Teacher manoj = new Teacher("Manoj", "077-1234567");
+//            Teacher aruni = new Teacher("Aruni", "011-1234567");
+//            Teacher manoj = new Teacher("Manoj", "077-1234567");
+//
+//            session.save(aruni);
+//            System.out.println("Aruni has been saved");
+//            System.out.println(aruni);
+//
+//            session.persist(manoj);
+//            System.out.println("Manoj has been saved");
+//            System.out.println(manoj);
+//
+//            Student pethum = new Student(2, "Pethum", "Galle", "011-456789123");
 
-            session.save(aruni);
-            System.out.println("Aruni has been saved");
-            System.out.println(aruni);
+            // transient/detach > detach
+//            Student dinusha2 = new Student(3, "Dinusha", "Gampaha", "033-456789123");
+//
+//            session.save(pethum);
+//            System.out.println("Pethum has been saved");
+//            System.out.println(pethum);
+//
+//            session.persist(dinusha2);
+//            System.out.println("Dinsuha has been saved");
+//            System.out.println(dinusha2);
 
-            session.persist(manoj);
-            System.out.println("Manoj has been saved");
-            System.out.println(manoj);
+            // transient, detach -> detach
+//            Student student = new Student(3, "Manoj", "Kandy", "011-1234567");
+//            session.persist(student);
+//            System.out.println("Manoj has been persisted");
 
-            Student pethum = new Student(2, "Pethum", "Galle", "011-456789123");
-            Student dinusha = new Student(3, "Dinusha", "Gampaha", "033-456789123");
-
-            session.save(pethum);
-            System.out.println("Pethum has been saved");
-            System.out.println(pethum);
-
-            session.persist(dinusha);
-            System.out.println("Dinsuha has been saved");
-            System.out.println(dinusha);
+            Student student = session.get(Student.class, 4);
+            session.detach(student);
+            // dinusha -> detach
+            student.setId(5); // transient
+            session.persist(student);
 
             session.getTransaction().commit();
 
