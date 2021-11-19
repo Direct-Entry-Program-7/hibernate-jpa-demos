f90524ea3a3ab5c6bbd53c6a13f99e159f8e09b3
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-19 10:50:08 +0530

    Add RefreshDemo

---
+++ 3-crud/src/main/java/lk/ijse/dep7/RefreshDemo.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep7;
+
+import lk.ijse.dep7.entity.Student;
+import lk.ijse.dep7.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class RefreshDemo {
+
+    public static void main(String[] args) {
+
+        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+             Session session = sessionFactory.openSession()) {
+
+            session.beginTransaction();
+
+            Student student = session.get(Student.class, 1);
+            System.out.println(student);
+            student.setContactNumber("071-1234567");
+            System.out.println(student);
+            session.refresh(student);
+            System.out.println(student);
+
+            session.getTransaction().commit();
+
+        }
+
+    }
+}
