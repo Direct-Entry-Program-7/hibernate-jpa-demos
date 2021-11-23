38fc08dcddeae3a302955125c7f56c5ac72eabd5
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-23 13:20:12 +0530

    OneToOneDemo3

--- 4-relationships/src/main/java/lk/ijse/dep7/OneToOneDemo3.java
+++ 4-relationships/src/main/java/lk/ijse/dep7/OneToOneDemo3.java
@@ -2,6 +2,7 @@ package lk.ijse.dep7;
 
 import lk.ijse.dep7.entity.Employee;
 import lk.ijse.dep7.entity.Spouse;
+import lk.ijse.dep7.entity.Vehicle;
 import lk.ijse.dep7.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -15,6 +16,15 @@ public class OneToOneDemo3 {
 
             session.beginTransaction();
 
+            Employee e002 = session.get(Employee.class, "E002");
+
+            Vehicle v001 = new Vehicle("V001", "Car");
+            Vehicle v002 = new Vehicle("V002", "Bike", e002);
+            Vehicle v003 = new Vehicle("V003", "Van");
+
+            session.save(v001);
+            session.save(v002);
+            session.save(v003);
 
             session.getTransaction().commit();
 
