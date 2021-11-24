08a87575f124a6cd2b92f57e7c14bc9786bf4e06
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-24 13:11:43 +0530

    Understanding Cascade Merge

--- 4-relationships/src/main/java/lk/ijse/dep7/entity/Order.java
+++ 4-relationships/src/main/java/lk/ijse/dep7/entity/Order.java
@@ -11,7 +11,7 @@ public class Order implements Serializable {
     private String id;
     @Column(nullable = false)
     private Date date;
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.DETACH)
     @JoinColumn(name = "customer_id", referencedColumnName = "id", nullable = false)
     private Customer customer;
 
