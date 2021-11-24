7f19b5de6147572c5969c97b4381208e42e17fdd
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-24 19:42:31 +0530

    Add OneToManyDemo11

--- 4-relationships/src/main/java/lk/ijse/dep7/OneToManyDemo11.java
+++ 4-relationships/src/main/java/lk/ijse/dep7/OneToManyDemo11.java
@@ -1,7 +1,7 @@
 package lk.ijse.dep7;
 
-import lk.ijse.dep7.entity.Customer2;
-import lk.ijse.dep7.entity.Order2;
+import lk.ijse.dep7.entity.*;
+import lk.ijse.dep7.entity.Class;
 import lk.ijse.dep7.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -18,6 +18,23 @@ public class OneToManyDemo11 {
         Session session = sessionFactory.openSession()){
 
             session.beginTransaction();
+
+            Class dep = new Class("DEP", "Direct Entry Program");
+            Class mad = new Class("MAD", "Mobile Application Developer");
+            Student pethum = new Student("S001", "Pethum", "Galle");
+            Student sovis = new Student("S002", "Sovis", "Moratuwa");
+            Student gayal = new Student("S003", "Gayal", "Gampaha");
+
+//            session.save(dep);
+            session.save(mad);
+//            session.save(pethum);
+            session.save(sovis);
+            session.save(gayal);
+
+            ClassStudent cs01 = new ClassStudent(new ClassStudentPK("DEP", "S001"), Date.valueOf("2021-01-05"), dep, pethum);
+            ClassStudent cs02 = new ClassStudent("DEP", "S002", Date.valueOf("2021-01-05"));
+            session.persist(cs01);
+            session.save(cs02);
 
             session.getTransaction().commit();
 
