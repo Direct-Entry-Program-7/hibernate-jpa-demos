196f156cbd5e08acae0f6c50d567c4ada4f06383
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-24 17:56:43 +0530

    Add OneToManyDemo7

--- 4-relationships/src/main/java/lk/ijse/dep7/OneToManyDemo7.java
+++ 4-relationships/src/main/java/lk/ijse/dep7/OneToManyDemo7.java
@@ -1,7 +1,9 @@
 package lk.ijse.dep7;
 
 import lk.ijse.dep7.entity.Customer;
+import lk.ijse.dep7.entity.Customer2;
 import lk.ijse.dep7.entity.Order;
+import lk.ijse.dep7.entity.Order2;
 import lk.ijse.dep7.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -19,7 +21,18 @@ public class OneToManyDemo7 {
 
             session.beginTransaction();
 
+            Customer2 c001 = new Customer2("C001", "Pethum", "Galle");
+            Customer2 c002 = new Customer2("C002", "Sovis", "Moratuwa");
 
+            Order2 od001 = new Order2("OD001", Date.valueOf("2021-11-25"));
+            Order2 od002 = new Order2("OD002", Date.valueOf("2021-11-20"),c001);
+            Order2 od003 = new Order2("OD003", Date.valueOf("2021-11-25"),c001);
+
+            session.save(c001);
+            session.save(c002);
+            session.save(od001);
+            session.save(od002);
+            session.save(od003);
 
             session.getTransaction().commit();
 
