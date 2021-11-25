8dd58d68821be2aca0f19000a27a5436d6b6ace7
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-25 18:15:17 +0530

    Update QueryDemo8

--- 5-queries/src/main/java/lk/ijse/dep7/QueryDemo8.java
+++ 5-queries/src/main/java/lk/ijse/dep7/QueryDemo8.java
@@ -4,6 +4,7 @@ import lk.ijse.dep7.entity.Customer;
 import lk.ijse.dep7.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.transform.Transformers;
 
 import java.util.List;
 
@@ -23,6 +24,16 @@ public class QueryDemo8 {
             // HQL
             List<Customer> customers2 = session.createQuery("FROM Customer c", Customer.class).list();
             customers2.forEach(System.out::println);
+
+            List<String> names = session.createQuery("SELECT c.name FROM Customer c", String.class).list();
+            names.forEach(System.out::println);
+
+            List<Detail> details = session.createQuery("SELECT new lk.ijse.dep7.Detail(c.name, c.address) FROM Customer c").list();
+            details.forEach(System.out::println);
+
+            List<Detail> details2 = session.createNativeQuery("SELECT c.name, c.address FROM customer c")
+                    .setResultTransformer(Transformers.aliasToBean(Detail.class)).list();
+            details2.forEach(System.out::println);
 
             session.getTransaction().commit();
 
