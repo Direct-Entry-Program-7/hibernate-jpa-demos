37336641daa8039c03d012188b0046fac5be3f0a
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-25 16:27:44 +0530

    Add QueryDemo1

--- 5-queries/src/main/java/lk/ijse/dep7/QueryDemo1.java
+++ 5-queries/src/main/java/lk/ijse/dep7/QueryDemo1.java
@@ -3,6 +3,10 @@ package lk.ijse.dep7;
 import lk.ijse.dep7.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 public class QueryDemo1 {
 
@@ -11,6 +15,18 @@ public class QueryDemo1 {
         try(SessionFactory sf = HibernateUtil.getSessionFactory();
         Session session = sf.openSession();){
 
+            NativeQuery nativeQuery = session.createNativeQuery("SELECT * FROM customer");
+            List<Object[]> list = nativeQuery.list();
+            Object[] row1 = list.get(0);
+            System.out.println("ID: " + row1[0]);
+            System.out.println("Name: " + row1[1]);
+            System.out.println("Address: " + row1[2]);
+
+            System.out.println("--------------------");
+
+            System.out.println("ID: " + list.get(1)[0]);
+            System.out.println("name: " + list.get(1)[1]);
+            System.out.println("name: " + list.get(1)[2]);
 
         }
 
