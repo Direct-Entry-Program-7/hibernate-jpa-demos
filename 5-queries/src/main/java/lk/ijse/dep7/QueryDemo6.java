2b6b36ab5c5a2d87849eb49d723a2820da8d6bca
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-25 17:24:50 +0530

    Add QueryDemo6

---
+++ 5-queries/src/main/java/lk/ijse/dep7/QueryDemo6.java
@@ -0,0 +1,36 @@
+package lk.ijse.dep7;
+
+import lk.ijse.dep7.entity.Customer;
+import lk.ijse.dep7.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
+
+public class QueryDemo6 {
+
+    public static void main(String[] args) {
+
+        try (SessionFactory sf = HibernateUtil.getSessionFactory();
+             Session session = sf.openSession()) {
+
+            session.beginTransaction();
+
+            NativeQuery query = session.createNativeQuery("INSERT INTO customer VALUES (:id, :name, :address)");
+            query.setParameter("id", "C004");
+            query.setParameter("name", "Aruni");
+            query.setParameter("address", "Matara");
+
+//            NativeQuery query = session.createNativeQuery("INSERT INTO customer VALUES (?3, ?2, ?1)");
+//            query.setParameter(1, "Panadura");
+//            query.setParameter(2, "Dulanga");
+//            query.setParameter(3, "C003");
+            int affectedRows = query.executeUpdate();
+
+            System.out.println("Has row been added? " + (affectedRows == 1));
+
+            session.getTransaction().commit();
+
+        }
+
+    }
+}
