d75af11aca2121aa5349863306c4abc91664a929
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-25 11:27:14 +0530

    Update the Actor entity to create the relationship

--- 4-relationships/src/main/java/lk/ijse/dep7/entity/Actor.java
+++ 4-relationships/src/main/java/lk/ijse/dep7/entity/Actor.java
@@ -1,12 +1,40 @@
 package lk.ijse.dep7.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Table(name = "actor")
 @Entity
 public class Actor extends SuperClass implements Serializable {
 
+    @ManyToMany
+    @JoinTable(name = "actor_movie",
+            joinColumns = @JoinColumn(name = "actor_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "movie_id", referencedColumnName = "id"))
+    private List<Movie> movieList;
+
+    public Actor() {
+    }
+
+    public Actor(String id, String name, List<Movie> movieList) {
+        super(id, name);
+        this.movieList = movieList;
+    }
+
+    public List<Movie> getMovieList() {
+        return movieList;
+    }
+
+    public void setMovieList(List<Movie> movieList) {
+        this.movieList = movieList;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "super=" + super.toString() + "," +
+                "movieList=" + movieList +
+                '}';
+    }
 }
