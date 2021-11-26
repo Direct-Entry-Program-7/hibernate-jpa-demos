5f17fc4fb96f3f97c4592e159f289c3fe54284d9
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-26 11:03:16 +0530

    Add QueryDemo13

---
+++ 5-queries/src/main/java/lk/ijse/dep7/QueryDemo13.java
@@ -0,0 +1,44 @@
+package lk.ijse.dep7;
+
+import lk.ijse.dep7.entity.CustomEntity;
+import lk.ijse.dep7.entity.Customer;
+import lk.ijse.dep7.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class QueryDemo13 {
+
+    public static void main(String[] args) {
+
+        try (SessionFactory sf = HibernateUtil.getSessionFactory();
+             Session session = sf.openSession()) {
+
+            session.beginTransaction();
+
+            /* Native Hibernate API */
+            List<Customer> customerList1 = session.getNamedQuery("findCustomersByAddress")
+                    .setParameter("q", "Gal")
+                    .list();
+
+            customerList1.forEach(System.out::println);
+
+            List<Customer> customerList2 = session.getNamedQuery("findCustomersByName")
+                    .setParameter(1, "Pet").list();
+
+            customerList2.forEach(System.out::println);
+
+            /* JPA API */
+            List<Customer> customerList3 = session.createNamedQuery("findCustomersByAddress").
+                    setParameter("q", "Gal").list();
+
+            customerList3.forEach(System.out::println);
+
+            session.getTransaction().commit();
+
+        }
+
+    }
+}
